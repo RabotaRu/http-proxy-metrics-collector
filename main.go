1ed08e1869514730b822759549fa17669dc0552f
zhiliaev <Zhilyaev.Dmitriy@gmail.com>
2022-08-11 13:34:59 +0300

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/url"
+	"os"
+
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var target = os.Getenv("TARGET_URL")
+
+func main() {
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	// Setup proxy
+	url1, err := url.Parse(target)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: url1,
+		},
+	}
+
+	// Setup prometheus collector
+	p := prometheus.NewPrometheus("echo", nil)
+	p.Use(e)
+
+	// Setup RR
+	e.Group("/").Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
+
+	e.Logger.Fatal(e.Start("0.0.0.0:3000"))
+}
